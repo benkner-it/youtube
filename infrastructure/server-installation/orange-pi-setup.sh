170338a78712e2ea38ab161491463b1663644465
Tobias Benkner <tobias.benkner@benkner-it.de>
2025-07-26 20:03:26 +0100

    cloudflare

--- infrastructure/server-installation/orange-pi-setup.sh
+++ infrastructure/server-installation/orange-pi-setup.sh
@@ -77,3 +77,19 @@ systemctl enable systemd-networkd --now
 # disable old
 systemctl disable NetworkManager --now
 systemctl disable resolvconf.service --now
+
+##########################################
+# Cloudflare
+##########################################
+
+# Add cloudflare gpg key
+mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add this repo to your apt repositories
+echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | tee /etc/apt/sources.list.d/cloudflared.list
+
+# install cloudflared
+apt update
+apt install -y cloudflared
+cloudflared service install $CLOUDFLARE_TOKEN
